9e1694bedc0a20e42a85dd64469ca3c152f439df
Akshay Kekuda <kekuda.akshay@gmail.com>
2020-09-18 14:27:49 +0530

    Update karatsuba_algo.py

--- karatsuba./karatsuba_algo.py
+++ karatsuba./karatsuba_algo.py
@@ -1,27 +1,25 @@
 # Implementing Karastuba Multiplication
 import math
-class kartsuba_algo(object):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
-    def karatsuba(self):
-    if self.x < 10 and self.y < 10: # return when both x and y are single digits
-        return x * y
+class karatsuba_algo:
 
-    n = max(len(str(self.x)) , len(str(self.y)))
-    m = math.ceil(n/2) #to take care of odd n
+    def compute_product(self, x, y):
 
-    a = self.x // (10 ** (m))
-    b = self.x % (10 ** (m))
-    c = self.y // (10 ** (m))
-    d = self.y % (10 ** (m))
+        if x < 10 and y < 10:     # return when both x and y are single digits
+            return x * y
 
-    ac = karatsuba(a, c)
-    bd = karatsuba(b, d)
-    ad_bc = karatsuba(a + b , c + d) - ac - bd
+        n = max(len(str(x)) , len(str(y)))
+        m = math.ceil(n/2) #to take care of odd n
 
-    return (ac * 10**(2*m) + ad_bc * 10**(m) + bd)
+        a = x // (10 ** (m))
+        b = x % (10 ** (m))
+        c = y // (10 ** (m))
+        d = y % (10 ** (m))
+
+        ac = self.compute_product(a, c)
+        bd = self.compute_product(b, d)
+        ad_bc = self.compute_product(a + b , c + d) - ac - bd
+        return (ac * 10**(2*m) + ad_bc * 10**(m) + bd)
 
 
 if __name__ == "__main__":
@@ -29,7 +27,8 @@ if __name__ == "__main__":
     x = int(input("Please enter the first number: \n"))
     y = int(input("Please enter the second number: \n"))
 
-    xy = karatsuba (x, y)
+    algo = karatsuba_algo()
+    xy = algo.compute_product(x,y)
     xy_true = x*y
     print("product of {} and {} by karatsuba is {} \n".format(x, y, xy))
     if xy == xy_true:
